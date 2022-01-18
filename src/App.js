e049222579140189fdcee7b5f095eee03656eaa3
marianaalves <marianaalves20@ua.pt>
2022-01-18 13:02:00 +0000

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <button className="btn btn-danger"></button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>

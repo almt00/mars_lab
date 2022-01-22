0d3a4961f69d914bfa553c0fb6cd0ed8543cd7c1
marianaalves <marianaalves20@ua.pt>
2022-01-22 16:31:24 +0000

    footer oh yeah

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCircle, faCopyright} from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 import Footer from './components/Footer';
+
+library.add(faCopyright, faCircle)
 
 function App() {
   return (

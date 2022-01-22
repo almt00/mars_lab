8601c97863a176f9b54c6893392706ca193b1c57
ana <almt00@ua.pt>
2022-01-22 11:39:17 +0000

    footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="">
-        <button className="btn btn-warning"></button>
-        <div className="fa"></div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Footer/>
     </div>
   );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,15 @@
 import "./Footer.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 function Footer() {
-    return (
-        <footer>
-  <p>Author: Hege Refsnes</p>
-  <p><a href="mailto:hege@example.com">hege@example.com</a></p>
-</footer>
-      );
+  return (
+    <footer className="">
+      <p>Author: Hege Refsnes</p>
+      <p>
+        <a href="mailto:hege@example.com">hege@example.com</a>
+      </p>
+    </footer>
+  );
 }
 export default Footer;

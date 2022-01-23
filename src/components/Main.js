1249aa17ddc35135f81b6c6e397c18df53617725
marianaalves <marianaalves20@ua.pt>
2022-01-23 13:55:12 +0000

    posicionamento cards comparar

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,15 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import FiveDays from "./FiveDays";
-import Recent from "./Recent";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FiveDays from './FiveDays';
+import Recent from './Recent';
+import Compare from './Compare';
 
 function Main() {
   return (
-    <div className="">
+    <div className=''>
       <Recent />
       <FiveDays />
-      <div className="row">
-        
+      <div className='row'>
+        <Compare />
       </div>
     </div>
   );

597a191712b68b944d072f7e6d66cb8d79e9e21e
ana <almt00@ua.pt>
2022-01-24 21:18:41 +0000

    formataçao (ignorar)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Navbar.css";
 import InputCity from "./InputCity";
-import switchEarth from '../assets/switch-elements-earth.svg';
-import switchMars from '../assets/switch-elements-mars.svg';
-
+import switchEarth from "../assets/switch-elements-earth.svg";
+import switchMars from "../assets/switch-elements-mars.svg";
 
 function Navbar() {
   let navItems = ["Recente", "5 dias", "Foto", "Comparar"];
@@ -11,30 +10,42 @@ function Navbar() {
 
   function showSwitch() {
     if (isMars != true) {
-      return <img id="switchMars" src={switchEarth} alt="Switch" className="switchPlanet w-50"></img>
+      return (
+        <img
+          id="switchMars"
+          src={switchEarth}
+          alt="Switch"
+          className="switchPlanet w-50"
+        ></img>
+      );
     } else {
-      return <img id="switchEarth"  src={switchMars} alt="Switch" className="switchPlanet w-50"></img>
+      return (
+        <img
+          id="switchEarth"
+          src={switchMars}
+          alt="Switch"
+          className="switchPlanet w-50"
+        ></img>
+      );
     }
   }
 
-  function searchbar (){
-    if (isMars===false) {
-      return <InputCity />
+  function searchbar() {
+    if (isMars === false) {
+      return <InputCity />;
     }
   }
 
   return (
     <nav className="navbar navbar-expand-sm px-3" id="navbarNav">
       <div className="container-fluid">
-        <form className="d-flex align-items-center">
-          {searchbar()} 
-        </form>
+        <form className="d-flex align-items-center">{searchbar()}</form>
         <div>
           <ul className="navbar-nav me-auto">
             {navItems.map((item) => {
               return (
                 <li className="nav-item" key={item}>
-                  <a className="nav-link text-white" href={"#"+item}>
+                  <a className="nav-link text-white" href={"#" + item}>
                     {item}
                   </a>
                 </li>

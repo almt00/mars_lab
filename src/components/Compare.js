5c9d20d78953573cdd3e77d3311ee542929c00b2
ana <almt00@ua.pt>
2022-01-24 11:23:04 +0000

    searchbar

--- src/components/Compare.js
+++ src/components/Compare.js
@@ -1,6 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './Compare.css';
-import InputCity from './InputCity';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Compare.css";
+import InputCity from "./InputCity";
 
 function Compare() {
   const isMars = true;
@@ -12,68 +12,66 @@ function Compare() {
   }
   return (
     <>
-      <div id="Comparar" className='row'>
-        <div className='col-6 margem'>
-          <div className='col-9'>
-            <div className='text-uppercase text-start ms-5 mt-5 mb-5'>
-              <h1 className=''>13 JAN 2022</h1>
+      <div id="Comparar" className="row">
+        <div className="col-6 margem">
+          <div className="col-9">
+            <div className="text-uppercase text-start ms-5 mt-5 mb-5">
+              <h1 className="">13 JAN 2022</h1>
               <p>
                 Marte é em média
-                <span className='fw-bolder'> 60,9º celsius </span>
+                <span className="fw-bolder"> 60,9º celsius </span>
                 mais frio que a terra
               </p>
             </div>
           </div>
-          <div className='col-7 ms-5 me-0'>
-            <div className='background-fosco temperaturas '>
-              <h5 className='text-uppercase mb-0'>Terra</h5>
+          <div className="col-7 ms-5 me-0">
+            <div className="background-fosco temperaturas ">
+              <h5 className="text-uppercase mb-0">Terra</h5>
               <p>Aveiro</p>
               <div>
-                <span className='m_texto'>
-                  <span className='tam_letra fw-bolder'>12,5</span>
+                <span className="m_texto">
+                  <span className="tam_letra fw-bolder">12,5</span>
                   <span>ºC</span>
                 </span>
                 <span>
-                  <span className='tam_letra fw-bolder'>12,5</span>
-                  <span className='align-text-top'>ºC</span>
+                  <span className="tam_letra fw-bolder">12,5</span>
+                  <span className="align-text-top">ºC</span>
                 </span>
                 <p>
-                  <span className='fw-bolder m_texto'>Mínima</span>
-                  <span className='fw-bolder'>Máxima</span>
+                  <span className="fw-bolder m_texto">Mínima</span>
+                  <span className="fw-bolder">Máxima</span>
                 </p>
               </div>
-              <h1 className='fw-bolder'>
-                12,5<span className='lead align-text-top'>ºC</span>
+              <h1 className="fw-bolder">
+                12,5<span className="lead align-text-top">ºC</span>
               </h1>
-              <h5 className='fw-bolder'>Média</h5>
+              <h5 className="fw-bolder">Média</h5>
             </div>
-            <div className='mt-3 text-start'>
-              <InputCity />
-            </div>
+            <div className="mt-3 text-start">{searchbar()}</div>
           </div>
         </div>
-        <div className='col-5 m-2 me-0 ms-auto mt-5'>
-          <div className='col-8 float-end me-5'>
-            <div className='background-fosco temperaturas'>
-              <h5 className='text-uppercase mb-4 mt-2'>Marte</h5>
+        <div className="col-5 m-2 me-0 ms-auto mt-5">
+          <div className="col-8 float-end me-5">
+            <div className="background-fosco temperaturas">
+              <h5 className="text-uppercase mb-4 mt-2">Marte</h5>
               <div>
-                <span className='m_texto'>
-                  <span className='tam_letra fw-bolder'>12,5</span>
+                <span className="m_texto">
+                  <span className="tam_letra fw-bolder">12,5</span>
                   <span>ºC</span>
                 </span>
                 <span>
-                  <span className='tam_letra fw-bolder'>12,5</span>
-                  <span className='align-text-top'>ºC</span>
+                  <span className="tam_letra fw-bolder">12,5</span>
+                  <span className="align-text-top">ºC</span>
                 </span>
                 <p>
-                  <span className='fw-bolder m_texto'>Mínima</span>
-                  <span className='fw-bolder'>Máxima</span>
+                  <span className="fw-bolder m_texto">Mínima</span>
+                  <span className="fw-bolder">Máxima</span>
                 </p>
               </div>
-              <h1 className='fw-bolder'>
-                12,5<span className='lead align-text-top'>ºC</span>
+              <h1 className="fw-bolder">
+                12,5<span className="lead align-text-top">ºC</span>
               </h1>
-              <h5 className='fw-bolder mb-2'>Média</h5>
+              <h5 className="fw-bolder mb-2">Média</h5>
             </div>
           </div>
         </div>

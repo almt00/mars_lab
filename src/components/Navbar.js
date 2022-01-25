197c84e164e4da1fc78e874f512973e4cdfe934a
ana <almt00@ua.pt>
2022-01-25 10:22:18 +0000

    nada

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
-import './Navbar.css';
-import InputCity from './InputCity';
-import React, { useContext } from 'react';
-import PlanetContext from '../contexts/PlanetContext';
-import PlanetSwitch from './PlanetSwitch';
+import "./Navbar.css";
+import InputCity from "./InputCity";
+import React, { useContext } from "react";
+import PlanetContext from "../contexts/PlanetContext";
+import PlanetSwitch from "./PlanetSwitch";
 
 function Navbar() {
   const { mars } = useContext(PlanetContext);
 
-  let navItems = ['Recente', '5 dias', 'Foto', 'Comparar'];
+  let navItems = ["Recente", "5 dias", "Foto", "Comparar"];
 
-  console.log('Navbar', mars);
+  console.log("Navbar", mars);
 
   function searchbar() {
     if (mars === false) {
@@ -18,22 +18,22 @@ function Navbar() {
   }
 
   return (
-    <nav className='navbar fixed-top navbar-expand-sm' id='navbarNav'>
-      <div className='container-fluid'>
-        <form className='d-flex align-items-center'>{searchbar()}</form>
+    <nav className="navbar fixed-top navbar-expand-sm" id="navbarNav">
+      <div className="container-fluid">
+        <form className="d-flex align-items-center">{searchbar()}</form>
         <div>
-          <ul className='navbar-nav me-auto'>
-            {navItems.map(item => {
+          <ul className="navbar-nav me-auto">
+            {navItems.map((item) => {
               return (
-                <li className='nav-item' key={item}>
-                  <a className='nav-link text-white ms-2' href={'#' + item}>
+                <li className="nav-item" key={item}>
+                  <a className="nav-link text-white ms-2" href={"#" + item}>
                     {item}
                   </a>
                 </li>
               );
             })}
-            <li className='nav-item'>
-              <a className='nav-link text-white px-0' href='#'>
+            <li className="nav-item">
+              <a className="nav-link text-white px-0" href="#">
                 <PlanetSwitch />
               </a>
             </li>
